e5fbfbe3921089532ca8b22770154dba8d24c0ce
hotan4866 <hotan4866>
2024-08-06 21:40:22 +0900

    스킬툴팁추가&인게임UI수정

---
+++ V Rising/Assets/VideoTest/Scripts/SkillTooltipUIConrtoller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SkillTooltipUIConrtoller : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Image basicAttackImg;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
