e0b2e10902b20c47fdae7e756587d474c870306b
hotan4866 <hotan4866>
2024-08-07 15:16:41 +0900

    스킬창 드래그시, 툴팁 추가

--- V Rising/Assets/VideoTest/Scripts/SkillTooltipUIConrtoller.cs
+++ V Rising/Assets/VideoTest/Scripts/SkillTooltipUIConrtoller.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class SkillTooltipUIConrtoller : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class SkillTooltipUIConrtoller : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
-    public Image basicAttackImg;
+    public GameObject tooltip;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("1");
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("2");
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("3");
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("4");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("5");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("들어옴");
+        tooltip.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("나감");
+        tooltip.SetActive(false);
     }
 }
