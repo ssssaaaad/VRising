2ea2320fe27dd3433f2899706b91dd3299d9a7ba
hotan4866 <hotan4866>
2024-08-07 13:42:59 +0900

    해상도 옵션 추가

--- V Rising/Assets/VideoTest/Scripts/OptionUIController.cs
+++ V Rising/Assets/VideoTest/Scripts/OptionUIController.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class OptionUIController : MonoBehaviour
 {
-    
+    [SerializeField]
+    List<RenderPipelineAsset> RenderPipelineAssets;
+
+    [SerializeField]
+    TMP_Dropdown Dropdown;
+
+    public void SetPipeline(int value)
+    {
+        QualitySettings.SetQualityLevel(value);
+        QualitySettings.renderPipeline = RenderPipelineAssets[value];
+    }
 }
