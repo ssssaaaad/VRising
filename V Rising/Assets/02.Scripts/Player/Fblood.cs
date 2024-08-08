db956331194947a5604cf0c51fad0d63de8ee836
RootLoop <phg0517@gmail.com>
2024-08-08 21:59:32 +0900

    AddIndicator

--- V Rising/Assets/02.Scripts/Player/Fblood.cs
+++ V Rising/Assets/02.Scripts/Player/Fblood.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class Fblood : MonoBehaviour
 {
+    private Playerstate PState;
+    private PlayerManager PManager;
+    private PlayerMove playerMove;
 
+    public HP_Scan_Range Scaner;
+
+    private bool fActive = false;
+    private Enemy closeEnemy;
 
     void Start()
     {
-        
+        PState = GetComponent<Playerstate>();
+        PManager = GetComponent<PlayerManager>();
+        playerMove = GetComponent<PlayerMove>();
     }
 
     public void F()
     {
-        // 범위 내에 들어온 적들 체력 확인
-        // 적들 체력이 30퍼 미만일 시 F흡혈 활성화
-        // F 입력시 
+        if (!fActive)
+            return;
+        // F 입력시 가장 가까운 대상 흡혈
+        transform.position = Scaner.closeEnemy.position - Scaner.closeEnemy.forward * 5;
+        // 흡혈중 플레이어 조작 불가
+        
+        // 흡혈중 흡혈 대상 이동 불가
         // 적 처형 및 체력 회복
+
+    }
+
+    public void FActive(bool canf)
+    {
+        fActive = canf;
     }
 }
