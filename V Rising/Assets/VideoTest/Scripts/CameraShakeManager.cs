db58dca10dbb53af2b2b4b6671fed7bf4d84684e
hotan4866 <hotan4866>
2024-08-09 20:01:55 +0900

    쉐이크 추가 수정

--- V Rising/Assets/VideoTest/Scripts/CameraShakeManager.cs
+++ V Rising/Assets/VideoTest/Scripts/CameraShakeManager.cs
@@ -8,17 +8,46 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
 using PrimeTween;
+using static UnityEditor.MaterialProperty;
+using Unity.VisualScripting;
+using Sequence = PrimeTween.Sequence;
 
 public class CameraShakeManager : MonoBehaviour
 {
     public static CameraShakeManager instance {  get; private set; }
 
     private new Camera camera;
-    [SerializeField, Range(0f, 1f)] float cameraShakeStrength = 0.4f;
 
-    public float shakeDuration = 0.5f;  // 쉐이킹 지속 시간
-    public float shakeMagnitude = 0.3f;  // 쉐이킹 강도
-    public float smoothTime = 0.1f;  // 쉐이킹의 부드러움 정도
+    [Header("흔들기 유형")]
+    [SerializeField] private Ease shakeEase;
+
+    [Header("강도")]
+    [SerializeField, Range(0f, 100f)] private float cameraShakeStrength = 0.4f; // 쉐이킹 강도
+
+    [Header("크기")]
+    [SerializeField, Range(0f, 10000f)] private float shakeMagnitude = 0.3f;  // 쉐이킹 크기
+
+    [Header("지속 시간")]
+    [SerializeField, Range(0f, 10f)] private float shakeDuration = 0.5f;  // 쉐이킹 지속 시간
+
+    [Header("X축 조절")]
+    [SerializeField, Range(0f, 10f)] private float offsetX = 0.5f;  // 쉐이킹 지속 시간
+
+    [Header("Y축 조절")]
+    [SerializeField, Range(0f, 10f)] private float offsetY = 0.5f;  // 쉐이킹 지속 시간
+
+    [Header("흔들기 회전 유형")]
+    [SerializeField] private Ease shakeRotEase;
+
+    [Header("X축 회전 조절")]
+    [SerializeField, Range(0f, 10f)] private float offsetRotX = 0.5f;  // 쉐이킹 지속 시간
+
+    [Header("Y축 회전 조절")]
+    [SerializeField, Range(0f, 10f)] private float offsetRotY = 0.5f;  // 쉐이킹 지속 시간
+
+    [Space]
+    [Header("사용안함")]
+    [SerializeField, Range(0f, 1f)] private float smoothTime = 0.1f;  // 쉐이킹의 부드러움 정도
 
     private Vector3 shakeOffset;  // 쉐이킹 오프셋
     private Vector3 velocity = Vector3.zero;  // 부드럽게 이동하기 위한 변수
@@ -52,11 +81,72 @@ public class CameraShakeManager : MonoBehaviour
     {
         //camera.DOShakePosition(.2f);
         //camera.DOShakeRotation(.1f);
-        Shake();
+
+
+        Transform transform = camera.transform;
+
+        float num = cameraShakeStrength * camera.orthographicSize * 0.03f;
+
+        Tween.ShakeLocalPosition(
+            transform, 
+            new ShakeSettings(
+                new Vector3(num + offsetX, num + offsetY),
+                shakeDuration,
+                shakeMagnitude, 
+                enableFalloff: true,
+                shakeEase, 
+                0f, 
+                1, 
+                startDelay, 
+                0f, 
+                false))
+            .Group(
+                Tween.ShakeLocalRotation(
+                    transform, 
+                    new ShakeSettings(
+                        new Vector3(offsetRotX, offsetRotY, cameraShakeStrength * 0.6f), 
+                        shakeDuration, 
+                        shakeMagnitude, 
+                        enableFalloff: true,
+                        shakeRotEase, 
+                        0f, 
+                        1, 
+                        startDelay,
+                        0f,
+                        false)));
+
+        //Tween.ShakeLocalPosition(camera.transform, new ShakeSettings(cameraShakeStrength * Vector3.forward, shakeDuration, shakeDuration, enableFalloff: true, Ease.Default, 0f, 1, startDelay, 1f, false));
+        //Shake();
+
     }
 
     public Sequence Shake(float startDelay = 0)
     {
+        
+        //Transform transform = camera.transform;
+
+        /*
+        if (camera.orthographic)
+        {
+            float num = cameraShakeStrength * camera.orthographicSize * 0.03f;
+            return Tween.ShakeLocalPosition(transform, new ShakeSettings(new Vector3(num, num), duration, shakeDuration, enableFalloff: true, Ease.Default, 0f, 1, startDelay, 1f, false)).Group(Tween.ShakeLocalRotation(transform, new ShakeSettings(new Vector3(0f, 0f, cameraShakeStrength * 0.6f), duration, shakeDuration, enableFalloff: true, Ease.Default, 0f, 1, startDelay, 0f, false)));
+        }
+         */
+
+
+        return Sequence.Create(Tween.ShakeLocalRotation(transform, new ShakeSettings(cameraShakeStrength * Vector3.one, shakeDuration, shakeDuration, enableFalloff: true, Ease.Default, 0f, 1, startDelay, 1f, false)));
+        
+
+        /*
+        Transform transform = camera.transform;
+        if (camera.orthographic)
+        {
+            float num = cameraShakeStrength * camera.orthographicSize * 0.03f;
+            return Tween.ShakeLocalPosition(transform, new ShakeSettings(new Vector3(num, num), duration, frequency, enableFalloff: true, Ease.Default, 0f, 1, startDelay, endDelay, useUnscaledTime)).Group(ShakeLocalRotation(transform, new ShakeSettings(new Vector3(0f, 0f, strengthFactor * 0.6f), duration, frequency, enableFalloff: true, Ease.Default, 0f, 1, startDelay, endDelay, useUnscaledTime)));
+        }
+
+        return Sequence.Create(Tween.ShakeLocalRotation(transform, new ShakeSettings(strengthFactor * Vector3.one, duration, frequency, enableFalloff: true, Ease.Default, 0f, 1, startDelay, endDelay, useUnscaledTime)));
+        */
 
         /*
          * 
@@ -73,7 +163,7 @@ public class CameraShakeManager : MonoBehaviour
             }
         */
 
-        return Tween.ShakeCamera(camera, cameraShakeStrength, startDelay: startDelay);
+        //return Tween.ShakeCamera(camera, cameraShakeStrength, duration: shakeDuration , frequency: shakeMagnitude, startDelay: startDelay);
     }
 
     public IEnumerator Shake(float duration, float magnitude)
