84403afbb34b29036bf05493fd13b70c2af75479
Sim <smtlacg11@gmail.com>
2024-08-15 19:56:16 +0900

    Bulid Debug

--- V Rising/Assets/VideoTest/Scenes/StartScenceCamera.cs
+++ V Rising/Assets/VideoTest/Scenes/StartScenceCamera.cs
@@ -2,7 +2,6 @@ using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro.EditorUtilities;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
